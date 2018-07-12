c63af2da56f6d245622bec5b293a5b04046298e5
Abhisakharkar <abhisheksakharkar@icloud.com>
2018-07-13 03:15:24 +0530

    added mandatory and license no functionality to retailer data

--- updateFullRetailerData.js
+++ updateFullRetailerData.js
@@ -1,27 +1,32 @@
-var update_full_retailer_data = function (req,res,authData) {
-  var mysql = require('mysql');
-  var retailerId=authData.data.retailerId;
-  var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "MH31eh@2964",
-    database: "hoverBackend"
-  });
+var update_full_retailer_data = function(req, res, authData) {
+  var retailerId = authData.data.retailerId;
+  if (req.body.mandatoryData!=null && req.body.shopActLicenseNo!=null){
+  var con = require('./databaseOptions')
+  var insertInAuth=false;
+    insertInAuth=true;
+  }
 
-  con.connect(function(err) {
+
+  makeQueryForRetailerDataUpdate = require('./makeQueryForRetailerDataUpdate');
+  var queryData = makeQueryForRetailerDataUpdate.makeQuery(req);
+  var updateVariables = queryData.updateVariables;
+  var variablesValues = queryData.variablesValues;
+  var updateData = queryData.updateData;
+  variablesValues.push(retailerId);
+  var sql = "UPDATE `RETAILER_DATA` SET " + updateVariables + " WHERE retailerId=?";
+  con.query(sql, variablesValues, function(err, rows) {
     if (err) {
       console.log(err);
-      console.log("error in database connection");
+      var myobj = {
+        responseFrom: "update_full_retailer_data",
+        update: false
+      }
+      console.log(myobj);
+      res.end(JSON.stringify(myobj));
     } else {
-      console.log("Connected to database!");
-      makeQueryForRetailerDataUpdate=require('./makeQueryForRetailerDataUpdate');
-      var queryData=makeQueryForRetailerDataUpdate.makeQuery(req);
-      var updateVariables=queryData.updateVariables;
-      var variablesValues=queryData.variablesValues;
-      var updateData=queryData.updateData;
-      variablesValues.push(retailerId);
-      var sql = "UPDATE `RETAILER_DATA` SET "+updateVariables+" WHERE retailerId=?";
-      con.query(sql,variablesValues, function(err, rows) {
+      if (insertInAuth) {
+        var sql = "UPDATE `RETAILER_AUTH` SET `mandatoryData` = ?, `shopActLicenseNo` = ? WHERE retailerId=?";
+        con.query(sql,[req.body.mandatoryData,req.body.shopActLicenseNo,retailerId], function(err, result) {
         if (err) {
           console.log(err);
           var myobj = {
@@ -34,46 +39,56 @@ var update_full_retailer_data = function (req,res,authData) {
           var myobj = {
             responseFrom: "update_full_retailer_data",
             update: true,
-            updateData:updateData
+            updateData: updateData,
+            mandatoryData:req.body.mandatoryData,
+            shopActLicenseNo:req.body.shopActLicenseNo
           }
-         console.log(myobj);
-         res.end(JSON.stringify(myobj));
+          console.log(myobj);
+          res.end(JSON.stringify(myobj));
         }
-      });
+        });
+      }else {
+        var myobj = {
+          responseFrom: "update_full_retailer_data",
+          update: true,
+          updateData: updateData
+        }
+        console.log(myobj);
+        res.end(JSON.stringify(myobj));
+      }
 
     }
   });
-
 }
 
-module.exports=update_full_retailer_data;
+module.exports = update_full_retailer_data;
 
 
 
 
 
 
-  // var req={
-  //   body:{
-  //
-  //     enterpriseName:'bckshbvc',
-  //     mobileNo:535485424,
-  //     addLine1:'kuhzdbfkuf',
-  //     sublocality1Id:1,
-  //     localityId:7,
-  //     proprietor:'hbmhzsbc',
-  //     latloc:75.8776,
-  //     longloc:67.8978,
-  //     deliveryStatus:0,
-  //     maxDeliveryDistanceInMeters:566,
-  //     maxFreeDeliveryDistanceInMeters:867,
-  //     chargePerHalfKiloMeterForDelivery:15.25,
-  //     openCloseIsManual:0,
-  //     shopOpenTime1:'09:33:35',
-  //     shopCloseTime1:'09:33:35',
-  //     shopOpenTime2:'09:33:35',
-  //     shopCloseTime2:'09:33:35',
-  //      currentState:0
-  //
-  //   }
-  // }
+// var req={
+//   body:{
+//
+//     enterpriseName:'bckshbvc',
+//     mobileNo:535485424,
+//     addLine1:'kuhzdbfkuf',
+//     sublocality1Id:1,
+//     localityId:7,
+//     proprietor:'hbmhzsbc',
+//     latloc:75.8776,
+//     longloc:67.8978,
+//     deliveryStatus:0,
+//     maxDeliveryDistanceInMeters:566,
+//     maxFreeDeliveryDistanceInMeters:867,
+//     chargePerHalfKiloMeterForDelivery:15.25,
+//     openCloseIsManual:0,
+//     shopOpenTime1:'09:33:35',
+//     shopCloseTime1:'09:33:35',
+//     shopOpenTime2:'09:33:35',
+//     shopCloseTime2:'09:33:35',
+//      currentState:0
+//
+//   }
+// }
